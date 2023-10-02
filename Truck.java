2c28028fac542d1d895514a6e48c50a9fd57a340
owowohoi <136990124+owowohoi@users.noreply.github.com>
2023-10-02 13:42:24 -0400

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
 
+public class Truck {
+    private int truckId;
+    private int maxWeight;
+    private int height;
+    private int length;
+    private int width;
+    private ArrayList<Box> boxes;
+
+    public void addBox(Box box) {
+
+    }
+    public void removeBox(int boxId) {
+
+    }
+    public int getLoadedWeight() {
+        return 0;
+    }
+    @Override
+    public String toString() {
+        return "";
+    }
+
+}
