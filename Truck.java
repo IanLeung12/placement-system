023fc285a624d7951bddd605c1745fc2ccf4bd6e
owowohoi <136990124+owowohoi@users.noreply.github.com>
2023-10-02 13:52:57 -0400

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -8,15 +8,36 @@ public class Truck {
     private int width;
     private ArrayList<Box> boxes;
 
+    // creating a truck with no boxes to be loaded
+    public Truck(int truckId, int maxWeight, int height, int length, int width) {
+        this.truckId = truckId;
+        this.maxWeight = maxWeight;
+        this.height = height;
+        this.length = length;
+        this.width = width;
+    }
+    // creating a truck with pending boxes to be loaded
+    public Truck(int truckId, int maxWeight, int height, int length, int width, ArrayList<Box> boxes) {
+        this.truckId = truckId;
+        this.maxWeight = maxWeight;
+        this.height = height;
+        this.length = length;
+        this.width = width;
+        this.boxes = boxes;
+    }
+    // add box to the pending boxes list
     public void addBox(Box box) {
 
     }
+    // remove box from the pending boxes list
     public void removeBox(int boxId) {
 
     }
+    // return th current loaded weight of the truck
     public int getLoadedWeight() {
         return 0;
     }
+    // return the truck as ______________________________________--
     @Override
     public String toString() {
         return "";
