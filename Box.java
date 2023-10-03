8283ad21c552c7529e2ca64400a2e6c6a37fa003
owowohoi <136990124+owowohoi@users.noreply.github.com>
2023-10-03 13:23:08 -0400

    Update Box.java

--- Box.java
+++ Box.java
@@ -11,14 +11,18 @@ public class Box {
     private int positionZInTruck;
     private Color color;
 
-    public Box(int id, int weight, int height, int length, int width) {
+    public Box(int id, int weight, int height, int length, int width, Color color) {
         this.boxID = id;
         this.weight = weight;
         this.height = height;
         this.length = length;
         this.width = width;
+        this.positionXInTruck = -1;
+        this.positionYInTruck = -1;
+        this.positionZInTruck = -1;
+        this.color = color;
     }
-    public Box(int id, int weight, int height, int length, int width, int x, int y, int z) {
+    public Box(int id, int weight, int height, int length, int width, int x, int y, int z, Color color) {
         this.boxID = id;
         this.weight = weight;
         this.height = height;
@@ -27,12 +31,16 @@ public class Box {
         this.positionXInTruck = x;
         this.positionYInTruck = y;
         this.positionZInTruck = z;
+        this.color = color;
     }
 
     public void rotate() {
         int tempLength = this.width;
         this.width = length;
         this.length = tempLength;
+    }
+    public int getWeight() {
+        return weight;
     }
 
 }
