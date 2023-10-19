e4d11488dfeb12f9856cab256e72351fc883a0d2
toxicface <47838537+toxicface@users.noreply.github.com>
2023-10-18 21:35:00 -0400

    other systenms

--- src/Main.java
+++ src/Main.java
@@ -15,108 +15,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         ArrayList<Box> boxes = new ArrayList<>();
-        Warehouse warehouse = new Warehouse(0, 100000, 100000, boxes, new ArrayList<Truck>());
+        Warehouse warehouse = new Warehouse(1, 99999, 20);
+        PlacementSystem placement = new PlacementSystem();
         ReceivingSystem receive = new ReceivingSystem(warehouse);
+        // DIsplay system not working rn cuz its in java 21
+        //DisplaySystem display = new DisplaySystem(warehouse, placement);
         receive.start();
-        int numBoxes;
-        int maxWeight;
-        int maxHeight;
-        int maxLength;
-        int maxWidth;
-        int truckMaxW;
-        int truckHeight;
-        int truckLength;
-        int truckWidth;
-
-        System.out.println("Type \"default\" for preset dimensions");
-        if (input.nextLine().equals("default")) {
-            numBoxes = 30;
-            maxWeight = 20;
-            maxHeight = 15;
-            maxLength = 15;
-            maxWidth = 15;
-            truckMaxW = 600;
-            truckHeight = 200;
-            truckLength = 100;
-            truckWidth = 20;
-        } else {
-            System.out.println("How many boxes are you adding?");
-            numBoxes = input.nextInt();
-            System.out.println("Enter max weight of a box?");
-            maxWeight = input.nextInt();
-            System.out.println("Enter max height of box?");
-            maxHeight = input.nextInt();
-            System.out.println("Enter max length of box?");
-            maxLength = input.nextInt();
-            System.out.println("Enter max width of box?");
-            maxWidth = input.nextInt();
-            System.out.println("Enter the max weight of the truck");
-            truckMaxW = input.nextInt();
-            System.out.println("Enter the height of the truck");
-            truckHeight = input.nextInt();
-            System.out.println("Enter the length of the truck");
-            truckLength = input.nextInt();
-            System.out.println("Enter the width of the truck");
-            truckWidth = input.nextInt();
-        }
-
-        for (int i = 0; i < numBoxes; i ++) {
-            boxes.add(new Box(i, (int) (Math.random() * maxWeight + 1), (int) (Math.random() * maxHeight + 1),
-                    (int) (Math.random() * maxLength + 1), (int) (Math.random() * maxWidth  +1), null));
-        }
-
-        Truck truck = new Truck(1, truckMaxW, truckHeight, truckLength, truckWidth);
-
-        PlacementSystem sys = new PlacementSystem();
-        sys.loadBoxesToTruck(boxes, truck);
-
-        int[][] space = sys.getTruckSpace();
-        printTruck(space);
-        System.out.println("The loaded boxes weigh " + truck.getLoadedWeight() +
-                " total out of the truck's " + truck.getMaxWeight() + " max weight");
-        System.out.println();
-
-        System.out.println("Truck");
-        for (Box box: truck.getBoxes()) {
-            System.out.println(box.getProps());
-        }
-
-        System.out.println(truck.getBoxes().size() + " boxes in truck.");
-        System.out.println("Leftover boxes");
-        for (Box box: boxes) {
-            System.out.println(box.getProps());
-        }
-
-        // Edit this to whatever you want
-        Box newBox = new Box(123, 1, 3, 2, 2, null);
-        boolean boxAdded = false;
-        while (!boxAdded) {
-            System.out.println("Type x and y coordinate to add 2x2 box");
-            boxAdded = sys.loadBoxToTruck(newBox, truck, input.nextInt(), input.nextInt());
-            space = sys.getTruckSpace();
-            printTruck(space);
-        }
-        input.close();
-
+        //display.runLoop();
     }
-    public static void printTruck(int[][] space) {
-        int counter = 0;
-        System.out.print("i: ");
-        for (int j = 0; j < space[0].length; j ++) {
-            System.out.print(j % 10);
-        }
-        System.out.println();
-        for (int i = 0; i < space.length; i ++) {
-            System.out.print(i % 10 + ": ");
-            for (int j = 0; j < space[i].length; j ++) {
-                System.out.print(space[i][j]);
-                if (space[i][j] == 0) {
-                    counter ++;
-                }
-            }
-            System.out.println();
-        }
-        System.out.println(counter + " empty spaces");
-    }
-
 }
+
--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,109 +1,215 @@
-/**
- * [Warehouse.java]
- * This class simulates a warehouse storing boxes and trucks
- * @author Ian Leung
- * @author Sfan Shen
- * @author Leon Yuan
- * @version 1.0
- * @since October 10, 2023
- */
-
-import java.io.File;
-import java.io.IOException;
 import java.awt.Graphics;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+
+/**
+ * Warehouse
+ * @author Michael Khart, Dilen De Silva
+ * ICS4UE
+ * @version 1.0 - 2023/10/09
+ * Warehouse stores all the boxes and trucks
+ * It is able to add and delete them if needed
+ */
 
 public class Warehouse {
-
-    final private int warehouseID;
-    final private ArrayList<Box> boxes;
-    final private ArrayList<Truck> trucks;
-
-    final private int MAX_BOXES;
-    final private int MAX_TRUCKS;
+    private int WarehouseID;
+    private int MAX_BOXES;
+    private int MAX_TRUCKS;
+    private ArrayList<Box> inventory = new ArrayList<>();
+    private ArrayList<Truck> trucks = new ArrayList<>();
 
     /**
      * Warehouse
-     * creates a warehouse with boxes and trucks
-     * @param warehouseID the warehouse id
-     * @param MAX_BOXES the max boxes in the warehouse
-     * @param MAX_TRUCKS the max trucks in your warehouse
-     * @param boxes the boxes in the warehouse
-     * @param trucks the trucks in the warehouse
+     * @param id - the id of warehosue
+     * @param maxBoxes - maxboxes of warehouse
+     * @param maxTrucks - max trucks of warehouse
      */
-    public Warehouse(int warehouseID, final int MAX_BOXES, final int MAX_TRUCKS, ArrayList<Box> boxes,
-            ArrayList<Truck> trucks) {
-        this.warehouseID = warehouseID;
-        this.boxes = boxes;
-        this.trucks = trucks;
-
-        this.MAX_BOXES = MAX_BOXES;
-        this.MAX_TRUCKS = MAX_TRUCKS;
+    Warehouse(int id, int maxBoxes, int maxTrucks) {
+        this.WarehouseID = id;
+        this.MAX_BOXES = maxBoxes;
+        this.MAX_TRUCKS = maxTrucks;
     }
+
 
     /**
      * addBox
-     * adds a box to the warehouse
-     * @param box the box to add
+     * adds a box to the warehouse inventory
+     * @param box - the box to add
      */
     public void addBox(Box box) {
-        this.boxes.add(box);
-    }
-
-    /**
-     * addTruck
-     * adds a truck to the warehouse
-     * @param truck the truck to add
-     */
-    public void addTruck(Truck truck) {
-        this.trucks.add(truck);
+        if (this.inventory.size() + 1 > this.MAX_BOXES) {
+            throw new RuntimeException("Too many boxes in warehouse");
+        } else {
+            this.inventory.add(box);
+        }
     }
 
     /**
      * removeBox
-     * removes a box from the warehouse
-     * @param boxID the id of the box
+     * removes box
+     * @param boxID - the id of which box to remove
      */
     public void removeBox(int boxID) {
-        for (int i = 0; i < this.boxes.size(); i++) {
-            if (this.boxes.get(i).getId() == boxID) {
-                this.boxes.remove(i);
-                i --;
+
+        for (Box box : this.inventory){
+            if(boxID == box.getId()){
+                this.inventory.remove(box);
             }
+        }
+
+        for (Truck truck : trucks) {
+            truck.removeBox(boxID);
+        }
+    }
+
+    /**
+     * addTruck
+     * adds the truck to warehouse list
+     * @param truck - truck to add
+     */
+    public void addTruck(Truck truck) {
+        if (this.trucks.size() + 1 > this.MAX_TRUCKS) {
+            throw new RuntimeException("Too many trucks in warehouse");
+        } else {
+            this.trucks.add(truck);
         }
     }
 
     /**
      * removeTruck
-     * removes a truck from the warehouse
-     * @param truckID the id of the truck
+     * removes the truck from list
+     * @param truckID - id of truck to remove
      */
     public void removeTruck(int truckID) {
-        for (int i = 0; i < this.trucks.size(); i++) {
-            if (this.trucks.get(i).getTruckId() == truckID) {
-                this.trucks.remove(i);
-                i --;
+
+        for (Truck truck : this.trucks) {
+            if (truck.getTruckId() == truckID) {
+                this.inventory.addAll(truck.getBoxes());
             }
         }
+
+        this.trucks.removeIf(truck -> truck.getTruckId() == truckID);
     }
 
+    // will be overridden
+    public void draw(Graphics g) {    }
 
-    public void loadBoxes(File f) throws IOException {
-
-    }
-
-    public void loadTrucks(File f) throws IOException {
-
-    }
-
-    public void draw(Graphics g) {
-
-    }
-
+    /**
+     * toString override
+     * will make a string out of warehouse and values - used in uplaoding info to files
+     * @return - the string object
+     */
     @Override
     public String toString() {
-        return null;
+        StringBuilder string = new StringBuilder("Warehouse"); // gets object type and its initation values into string
+        int[] values = ReceivingSystem.asArray(this);
+
+        for (int value : values) { // adds all values
+            string.append(" ");
+            string.append(value);
+        }
+
+
+        for (Box box : this.inventory) { // adds all string representation of boxes in inventory to string
+            string.append("\n");
+            string.append(ReceivingSystem.toStringForWarehouse(box));
+        }
+
+        for (Truck truck : this.trucks) { // adds all string representation of trucks and their boxes to string
+            string.append("\n");
+            string.append(truck.toString());
+        }
+
+        return string.toString();
     }
+
+    /**
+     * getWarehouseID
+     * Gets the ID of the warehouse.
+     * @return - The warehouse ID
+     */
+    public int getWarehouseID() {
+        return WarehouseID;
+    }
+
+    /**
+     * setWarehouseID
+     * Sets the ID of the warehouse.
+     * @param warehouseID - The new warehouse ID to set
+     */
+    public void setWarehouseID(int warehouseID) {
+        WarehouseID = warehouseID;
+    }
+
+    /**
+     * getMAX_BOXES
+     * Gets the maximum number of boxes allowed in the warehouse.
+     * @return - The maximum number of boxes
+     */
+    public int getMAX_BOXES() {
+        return MAX_BOXES;
+    }
+
+    /**
+     * setMAX_BOXES
+     * Sets the maximum number of boxes allowed in the warehouse.
+     * @param MAX_BOXES - The new maximum number of boxes to set
+     */
+    public void setMAX_BOXES(int MAX_BOXES) {
+        this.MAX_BOXES = MAX_BOXES;
+    }
+
+    /**
+     * getMAX_TRUCKS
+     * Gets the maximum number of trucks allowed in the warehouse.
+     * @return - The maximum number of trucks
+     */
+    public int getMAX_TRUCKS() {
+        return MAX_TRUCKS;
+    }
+
+    /**
+     * setMAX_TRUCKS
+     * Sets the maximum number of trucks allowed in the warehouse.
+     * @param MAX_TRUCKS - The new maximum number of trucks to set
+     */
+    public void setMAX_TRUCKS(int MAX_TRUCKS) {
+        this.MAX_TRUCKS = MAX_TRUCKS;
+    }
+
+    /**
+     * getInventory
+     * Gets the inventory of boxes in the warehouse.
+     * @return - A list of boxes in the inventory
+     */
+    public ArrayList<Box> getInventory() {
+        return inventory;
+    }
+
+    /**
+     * setInventory
+     * Sets the inventory of boxes in the warehouse.
+     * @param inventory - The new inventory of boxes to set
+     */
+    public void setInventory(ArrayList<Box> inventory) {
+        this.inventory = inventory;
+    }
+
+    /**
+     * getTrucks
+     * Gets the list of trucks in the warehouse.
+     * @return - A list of trucks
+     */
+    public ArrayList<Truck> getTrucks() {
+        return trucks;
+    }
+
+    /**
+     * setTrucks
+     * Sets the list of trucks in the warehouse.
+     * @param trucks - The new list of trucks to set
+     */
+    public void setTrucks(ArrayList<Truck> trucks) {
+        this.trucks = trucks;
+    }
+
 }
